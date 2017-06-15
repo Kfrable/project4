797cbfc8c620a61c787416e70fb003b84b40692d
Kfrable <kcfrable@gmail.com>
2017-06-15 09:23:03 -0400

    commit

--- beverage-pairing/src/components/Wines.js
+++ beverage-pairing/src/components/Wines.js
@@ -1,8 +1,7 @@
-import React,{Component} from 'react';
+import React from 'react';
 import {
-  Image,imageResponsiveInstance,responsive,Navbar,Form,FormGroup,FormControl,Button,Well
+  Image,imageResponsiveInstance,responsive
 } from 'react-bootstrap';
-import axios from 'axios';
 
 const Style ={
 	'border-radius':0,
@@ -32,64 +31,21 @@ const Style2 ={
 }
 
    
-class Wines extends Component(){
-	
-	
-	press(event){
-		let value = document.getElementById('put').value
-		console.log(value)
-		let url = 'https://stormy-thicket-68972.herokuapp.com/wines';
-    axios.get(url).then((res) => {
-      console.log('whats new return-->', res.data.beers);
-      let alldata = res.data.beers
-      for(let i = 0; i < alldata.length; i++){
-      		let name = document.createElement('h1');
-      		name.innerHTML=alldata[i].name
-      		let des = document.createElement('h2');
-      		des.innerHTML=alldata[i].description
-      		let style = document.createElement('h3');
-      		style.innerHTML=alldata[i].style
-      		let picture = document.createElement('div');
-      		picture.setAttribute('id','pic');
-      		let hold = document.getElementById('main')
-      		let ul = document.getElementById('holder')
-
-      		name.appendChild(des);
-      		name.appendChild(style);
-      		name.appendChild(picture);
-      		hold.appendChild(name);
-      }
-	})
-}
-	render(){
+function Wines(){
 	return(
 		<div>
-		<div id='main'>
-				
-			<center>
-			<Navbar.Form>
-	        <FormGroup>
-	        
-	          <FormControl id='put' type="text" placeholder="Search" />
-	        </FormGroup>
-
-	        <Button onClick={this.press} type="submit">Submit</Button>
-	      </Navbar.Form>
-	      </center>
 		<div style={Style}>
 		<Image style={imgs}  src="http://www.quoteambition.com/wp-content/uploads/2017/03/dogs.jpg" responsive />
 		</div>
-		</div>
-		</div>
-
 
 			
 
-		
+		<div >
+		<Image style={Style2}  src="http://www.quoteambition.com/wp-content/uploads/2017/03/dogs.jpg" responsive />
+		</div>
 
-		
+		</div>
 	)
-}
 }
 
 export default Wines
